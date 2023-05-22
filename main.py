34f451db63ce07ee3b7fdc6cab70b965dddf5eac
Mike <s02200482@gse.cs.msu.ru>
2023-05-22 12:37:37 +0300

    Added game log output

--- main.py
+++ main.py
@@ -68,6 +68,7 @@ def check_multiplex(n, input_str):
 
 n = int(input('Введите число "адресных" переменных n: '))
 print('Пример для ввода при n = 1: (!x0&y0|x0&y1)')
+print('Пример для ввода при n = 2: (y00|x0|x1)&(y01|x0|!x1)&(y10|!x0|x1)&(y11|!x0|!x1)')
 form = input(f'Введите формулу, реализующую мультиплексорную функцию от {n} переменных:\n')
 
 while not check_correct(n, form):
@@ -78,4 +79,18 @@ while not check_multiplex(n, form):
     print('Формула задана корректно, но не реализует мультиплексорную функцию, попробуйте ввести еще раз:')
     form = input()
 
-print(f'Формула задана корректно и реализует мультиплексорную функцию от {n} переменных')
+print(f'Формула задана корректно и реализует мультиплексорную функцию от {n} переменных\n')
+
+print("Protocol super trivial:")
+for i in range(n):
+    print(f"Alice sends a{i} to Bob")
+
+for i in range(n):
+    print(f"Bob sends b{i} to Alice")
+print("They find the difference with n rounds and 2*n algorithm complexity\n")
+
+print("Or if the protocol is trivial:")
+for i in range(n):
+    print(f"Alice sends a{i} to Bob")
+print("Bob is thinking log(n) times and sends b0 to Alice")
+print("And they find the difference with n rounds and n + log(n) algorithm complexity\n")
